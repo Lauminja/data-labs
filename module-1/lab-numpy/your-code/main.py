34be265aa5cf350aa26a43c5fddb7fbb79b9c3f5
Lauminja <lauma.ustupa@gmail.com>
2019-07-06 14:48:03 +0200

    lab Numpy & Pandas

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,76 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.random((2,3,5))
+??
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.full((5,2,3),1)
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
-
+#Yes:
+(a.size) == (b.size)
 
 #8. Are you able to add a and b? Why or why not?
-
-
+#No, because they don't have the same number of elements in each list
+np.add(a,b)
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.transpose([1,2,0])
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+#Because a and c have the same structure, same number of elements in each list
+d = np.add(a,c)
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+# Each value in d is greather by 1 than the value in the same position in a? This is because the values in c that we added to a, were all 1.
+print(a)
+print(d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = np.multiply(a,c)
 
 
 #13. Does e equal to a? Why or why not?
-
+#Yes, because all values in c are 1, any value multiplied by 1 stays the same. So values in a multiplied by 1, will stay the same.
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
-
-
+print(d_max)
+print(d_min)
+print(d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2,3,5))
+print(f)
 
 
 
